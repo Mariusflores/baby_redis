129aefffbbcd8ac2cd4c19a2930e07842bda58a5
mariusflores <mfloresdal@gmail.com>
2026-05-02 01:21:46 +0200

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-if ! ps aux | grep -q "[b]aby-redis.jar"; then
-	echo "Building package..."
+cd "$(dirname "$0")" || exit 1
+
+
+if ! ps aux | grep -q "[b]aby-redis.jar"; then	
+echo "Building package..."
+
 	mvn clean package
+
 	echo "Starting Baby Redis..."
 	java -jar target/baby-redis.jar &
 else
